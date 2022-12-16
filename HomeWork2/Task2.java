2907887a5c58cd67ca5bcd15b0d938193b7b90cb
KonstantinSuvorov <106460617+KonstantinSuvorov@users.noreply.github.com>
2022-12-16 12:53:21 +0300

    Update Task2.java

--- HomeWork2/Task2.java
+++ HomeWork2/Task2.java
@@ -5,7 +5,7 @@ public class Task2 {
     
     public static void main(String[] args) {
         try {
-            int d = 0;
+            int d = 1;
             int[] intArray = {0, 1, 2, 3, 4, 5, 1, 0, 10};
             double catchedRes1 = intArray[8] / d;
             System.out.println("catchedRes1 = " + catchedRes1);
